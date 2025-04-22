b192a8e26866bc4ab739e7f66ea5258ba9f72f6e
jonasluis <jonasluis66@gmail.com>
2025-04-22 11:36:03 -0300

    feat: Criar uma aplicação de consulta de API viaCEP

---
+++ src/ConsultaCep.java
@@ -0,0 +1,31 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaCep {
+
+
+    public Endereco buscaEndereco(String cep) {
+        try {
+            URI enderecoURL = URI.create("https://viacep.com.br/ws/" + cep + "/json/");
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(enderecoURL)
+                    .build();
+
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String responseBody = response.body();
+
+            return new Gson().fromJson(responseBody, Endereco.class);
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("CEP Inválido!");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
+        ConsultaCep consultaCep = new ConsultaCep();
+        Endereco novoEndereco = consultaCep.buscaEndereco("22763010");
+        System.out.println(novoEndereco);
     }
 }
